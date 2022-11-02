120c9f3bd13233ef05b1eec01d42aac013927b08
Shuhei Shibahara <Shoehey06@gmail.com>
2022-11-02 15:12:18 -0700

    show page state working

--- frontend/src/components/ProductPage/Sideinfo.js
+++ frontend/src/components/ProductPage/Sideinfo.js
@@ -1,10 +1,35 @@
 import './Sideinfo.css'
 import star from "../../images/star.png"
+import favorite from '../../images/favorite_icon.png'
+import { useState, useEffect } from 'react'
+import { useRef } from 'react'
+
 
 const Sideinfo = ({product}) => {
 
   const colors = product.color.split(' ')
   const sizes = product.size.split(' ')
+  // const borderDesign = useRef()
+
+  const [pickedColor, setPickedColor] = useState(colors[0])
+  const [selectedColor, setSelectedColor] = useState(0)
+  const [pickedSize, setPickedSize] = useState(sizes[2]) 
+  const [selectedSize, setSelectedSize] = useState(2)
+
+  const handleColorChange = (color, i) => (e) => {
+    e.preventDefault();
+    console.log(color, i)
+    setSelectedColor(i);
+    setPickedColor(color);
+  }
+
+  const handleSizeChange = (size, j) => (e) => {
+    e.preventDefault();
+    setSelectedSize(() => j);
+    setPickedSize(size);
+  }
+  
+
 
   return(
     <>
@@ -23,14 +48,18 @@ const Sideinfo = ({product}) => {
           </div>
         </div>
         <div className='side_bar_color_title'>Color</div>
-        <div className='side_bar_clicked_color'>{product.color}</div>
+        <div className='side_bar_clicked_color'>{pickedColor}</div>
         <div className='side_bar_color_circle_main'>
           <div className='side_bar_color_container'>
-            {colors.map(color =>
+            {colors.map((color, i) =>
             (<>
               <div className='color_container'>
-                <button className='future_onclick_for_color'>
-                  <div className='color_image_styling'>
+                {/* <button onClick={handleColorChange(color, i)} className={`future_onclick_for_color ${parseInt(i) === selectedColor ? 'selected_color' : ''}`}> */}
+                <button onClick={handleColorChange(color, i)} className='future_onclick_for_color'>
+
+                  {/* <div className={`color_image_styling`} id={`color-${i}`} key={i}> */}
+
+                  <div className={`color_image_styling ${parseInt(i) === selectedColor ? 'selected_color' : ''}`} id={`color-${i}`} key={i}>
                     <img src="https://sick-snow.s3.us-west-2.amazonaws.com/m-yeti-main.png" className='color_image' title={color}/>
                   </div>
                 </button>
@@ -40,16 +69,16 @@ const Sideinfo = ({product}) => {
             )}
           </div>
         </div>
-        <div class="side_bar_line_container">
-          <div class="line"></div>
+        <div className="side_bar_line_container">
+          <div className="line"></div>
         </div>
         <div className='side_bar_size_circle_main'>
           <div className='side_bar_size_circle_container'>
               <div>
                 <nav className='size_button_nav'>
-                  {sizes.map(size=>
+                  {sizes.map((size, j)=>
                   (<>
-                    <button className='size_button'>{size}</button>
+                    <button onClick={handleSizeChange(size, j)} className={`size_button ${parseInt(j) === selectedSize ? 'selected_size' : ''}`} key={j}>{size}</button>
                   </>) 
                   )}
 
@@ -59,10 +88,27 @@ const Sideinfo = ({product}) => {
         </div>
         <div className='side_bar_cart_favorite_main'>
           <button className='side_bar_shopping_cart_button'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20"><g fill="none" fill-rule="evenodd"><path stroke="currentColor" stroke-width="2" d="M3.5 0v13.65h10.182L17.5 4.095h-14"></path><ellipse fill="currentColor" fill-rule="nonzero" cx="4" cy="17.9" rx="1.5" ry="1.575"></ellipse><ellipse fill="currentColor" fill-rule="nonzero" cx="12" cy="17.9" rx="1.5" ry="1.575"></ellipse></g></svg>
-            <span class="nvrF" data-e2e="CURP">$ {product.price} USD</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20"><g fill="none" fillRule="evenodd"><path stroke="currentColor" strokeWidth="2" d="M3.5 0v13.65h10.182L17.5 4.095h-14"></path><ellipse fill="currentColor" fillRule="nonzero" cx="4" cy="17.9" rx="1.5" ry="1.575"></ellipse><ellipse fill="currentColor" fillRule="nonzero" cx="12" cy="17.9" rx="1.5" ry="1.575"></ellipse></g></svg>
+            <span className="cart_price_text" data-e2e="CURP">$ {product.price} USD</span>
+          </button>
+          <button className='side_bar_favorite_button'>
+            <img src={favorite} className='favorite_icon' />
           </button>
         </div>
+        <ul className="side_bar_list_amenities">
+          <li>
+            <span className="list_check_mark"></span>
+            <span>Free shipping and free returns</span>
+          </li>
+          <li>
+            <span className="list_check_mark"></span>
+            <span>30 day right of return</span>
+          </li>
+          <li>
+            <span className="list_check_mark"></span>
+            <span>Delivery time 2-6 business days</span>
+          </li>
+        </ul>
       </div>
     </>
   )
