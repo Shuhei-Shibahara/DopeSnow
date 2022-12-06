92917d6a3f5062751f3507ac78c88b22818c00d0
Shuhei Shibahara <Shoehey06@gmail.com>
2022-12-06 10:35:59 +0900

    scroll to top

--- frontend/src/components/ProductIndexPage/index.js
+++ frontend/src/components/ProductIndexPage/index.js
@@ -13,6 +13,10 @@ export default function ProductIndex(){
   useEffect(() => {
     dispatch(fetchProducts(gender, category))
   },[dispatch, gender, category])
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0)
+  }
   
   return(
     <>
@@ -20,20 +24,22 @@ export default function ProductIndex(){
         {products.map(product =>
           (<>
              <article className="indiviual_product_container">
-              <Link to={`/product/${product.id}`} className="individual_product">
-                <div className="individual_product_image_container">
-                  <div className="image_container_spacing"> 
-                  <img src={product.imgUrls[0]} className="product_main_image" />
-                  {/* <img src={"https://sick-snow.s3.us-west-2.amazonaws.com/Screen+Shot+2022-11-01+at+9.24.02+AM.png"} className="product_main_image"/> */}
+              <div onClick={scrollToTop}>
+                <Link to={`/product/${product.id}`} className="individual_product">
+                  <div className="individual_product_image_container">
+                    <div className="image_container_spacing"> 
+                    <img src={product.imgUrls[0]} className="product_main_image" />
+                    </div>
+                  </div>
+                <div className="product_text_whole_container">
+                  <div className="product_price_name_container">
+                    <div className="product_name_index">{product.name}</div>
+                    <div className="product_price_index"><span>$ {product.price} USD&nbsp;</span></div>
                   </div>
                 </div>
-              <div className="product_text_whole_container">
-                <div className="product_price_name_container">
-                  <div className="product_name_index">{product.name}</div>
-                  <div className="product_price_index"><span>$ {product.price} USD&nbsp;</span></div>
-                </div>
+                </Link>
+
               </div>
-              </Link>
 
              </article>
           </>)
